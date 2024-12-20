cd348f5104c669933deefc15e6f92158b2182c92
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-12-20 13:50:30 +0530

    exe

--- bank_accounts.py
+++ bank_accounts.py
@@ -1,5 +1,8 @@
 class BalanceException(Exception) :
     pass
+
+class BankAccount:
+    
     def __init__(self, initial_amount,acct_name):
         self.balance = initial_amount
         self.name = acct_name
@@ -21,17 +24,51 @@ class BalanceException(Exception) :
             return
         else:
             raise BalanceException(
-                f"\n Sorry,account '{self.name}' only has a balance of ${self.balance:.2f}"
+                f"\n Sorry, account '{self.name}' only has a balance of ${self.balance:.2f}"
             )
     def withdraw(self, amount):
         try:
-            self.viable_transaction(amount)
+            self.viable_transaction (amount)
             self.balance = self.balance - amount
             print("\n Withdraw complete.")
-        except:
+            self.get_balance()
+        except BalanceException as error:
+            print(f'\n Withdraw interrupted: {error}')
+    def transfer(self,amount,account):
+        try:
+            print('\n*************\n\nBeginning Transfer..')
+            self.viable_transaction(amount)
+            self.withdraw(amount)
+            account.deposit(amount)
 
+        except  BalanceException as error:
+            print(f'\n Transfer interrupted. {error}')
 
+class InterestRewardsAcct(BankAccount):
+    def deposit(self,amount):
+        self.balance = self.balance + (amount * 1.05)
+        print("/n Deposit complete.")
+        self.get_balance()
+
+class SavingsAct(InterestRewardsAcct):
+    def __init__(self, initial_amount,acct_name):
+        super().__init__(initial_amount,acct_name)
+        self.fee = 5
+
+    def withdraw(self,amount):
+        try:
+            self.viable_transaction(amount + self.fee)
+            self.balance = self.balance - (amount + self.fee)
+            print("\n Withdraw completed.")
+            self.get_balance()
+        except BalanceException as error:
+            print(f'\nWithdraw interrupted: {error}')
 
 
 
     
+
+
+
+
+        
--- oops.py
+++ oops.py
@@ -1,7 +1,7 @@
 from bank_accounts import *
 
-Vinodhini = BankAccount(1000, "Vinodhini")
-Abirami = BankAccount(2000, "Abirami")
+Vinodhini = BalanceException(1000, "Vinodhini")
+Abirami = BalanceException(2000, "Abirami")
 
 Vinodhini.get_balance()
 Abirami.get_balance()
@@ -9,4 +9,14 @@ Abirami.get_balance()
 Vinodhini.deposit(500)
 Abirami.deposit(2)
 
- 
+Vinodhini.withdraw(100)
+Vinodhini.transfer(200,Abirami)
+
+Mani = InterestRewardsAcct(100, "Mani")
+Mani.get_balance()
+Mani.deposit(100)
+
+Kani = SavingsAct(1000, "kani")
+Kani.get_balance()
+Kani.withdraw(100)
+Kani.transfer(5, Vinodhini)
